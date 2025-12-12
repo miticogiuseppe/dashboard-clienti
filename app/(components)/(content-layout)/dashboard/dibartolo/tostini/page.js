f7dd6404028f9c81a2c8bbb1f8fe95fb75c93414
miticogiuseppe <miticogiuseppe@gmail.com>
2025-12-12 17:18:33 +0100

    _

--- app/(components)/(content-layout)/dashboard/dibartolo/tostini/page.js
+++ app/(components)/(content-layout)/dashboard/dibartolo/tostini/page.js
@@ -88,6 +88,8 @@ export default function PaginaTostini() {
     return !data || !data2;
   }, [data, data2]);
 
+  console.log("data2:", data2);
+
   // Stato per TS Azienda
   const computedDateTS = useMemo(() => {
     if (pickerDateTS) return pickerDateTS;
@@ -239,6 +241,7 @@ export default function PaginaTostini() {
               </Card>
             </Col>
           </Row>
+
           <Row className="mt-4">
             <Col xl={6}>
               <AppmerceTable
