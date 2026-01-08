f2e1e3a921e16bb55ca3fb61f5ade3270c3ac34f
miticogiuseppe <miticogiuseppe@gmail.com>
2026-01-08 18:46:03 +0100

    _

--- app/(components)/(content-layout)/dashboard/copral/calendario/page.js
+++ app/(components)/(content-layout)/dashboard/copral/calendario/page.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [orders, setOrders] = useState([]);
+  const [fileDate, setFileDate] = useState(undefined);
 
   useEffect(() => {
     // Carica automaticamente il file Excel
@@ -13,6 +14,11 @@ export default function Home() {
         "/api/fetch-excel-json?id=APPMERCE-000&sheet=APPMERCE-000_1"
       );
       const json = await response.json();
+
+      if (json.lwt) {
+        setFileDate(new Date(json.lwt));
+      }
+
       let newOrders = json.data;
       newOrders = parseDates(newOrders, ["Data Cons."]);
       setOrders(newOrders);
@@ -23,7 +29,7 @@ export default function Home() {
 
   return (
     <div className="container mx-auto p-4">
-      <OrderCalendar data={orders} />
+      <OrderCalendar data={orders} fileDate={fileDate} />
     </div>
   );
 }
